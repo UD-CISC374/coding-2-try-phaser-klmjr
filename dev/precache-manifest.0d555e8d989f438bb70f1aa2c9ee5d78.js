8cc925b9650d2baff2e68fcd05dbd10e3906daf4
klmjr <klmjr@udel.edu>
2020-02-27 22:23:28 -0500

    Updates

---
+++ dev/precache-manifest.0d555e8d989f438bb70f1aa2c9ee5d78.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-klmjr/devassets/citations.md"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-klmjr/devassets/images/background.png"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-klmjr/devassets/images/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-klmjr/devassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-klmjr/devassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-klmjr/devassets/images/ship3.png"
+  },
+  {
+    "revision": "f3dc728423b6f07143da",
+    "url": "/coding-2-try-phaser-klmjr/devgen.main.1fabc90be5fbf2f0911a.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-klmjr/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "887c8467e04513a01ff5c7c8340aab97",
+    "url": "/coding-2-try-phaser-klmjr/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-klmjr/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-klmjr/devsw.js"
+  }
+]);
