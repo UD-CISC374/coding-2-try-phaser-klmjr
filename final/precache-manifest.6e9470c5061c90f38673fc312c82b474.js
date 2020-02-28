598d1e4f6caa84fec54bd89a389d0ab7551ff6c0
klmjr <klmjr@udel.edu>
2020-02-28 15:09:06 -0500

    Updates

---
+++ final/precache-manifest.6e9470c5061c90f38673fc312c82b474.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/citations.md"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/images/background.png"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/images/colors.png"
+  },
+  {
+    "revision": "bf85dd90d9de42c97b646ed6f824c688",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/images/red_background.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/images/ship3.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-klmjr/finalassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "823a9da2793ee67079f6",
+    "url": "/coding-2-try-phaser-klmjr/finalgen.main.0fd4e0217d4afd99f6c4.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-klmjr/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "405ea791a1b9e167d5a96755a1b29d89",
+    "url": "/coding-2-try-phaser-klmjr/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-klmjr/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-klmjr/finalsw.js"
+  }
+]);
---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-klmjr/finalprecache-manifest.6e9470c5061c90f38673fc312c82b474.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
